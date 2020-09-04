a38a9d969b604b2b206932ec228d15cc31aae37a
zaid-syed <70218862+zaid-syed@users.noreply.github.com>
2020-09-04 15:32:42 -0700

    Delete day3-batch7-assignment1-python-letsupgrade.py

--- day3-batch7-assignment1-python-letsupgrade.py
+++
@@ -1,8 +0,0 @@
-# day3-batch7-assignment1-python-letsupgrade
-altitude= int(input("altitude of plane"))
-if altitude<=1000:
-	print("safe land")
-elif altitude<=5000:
-	print("come to 1000")
-else:		
-	print("go around and try again")
