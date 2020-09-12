5b472bb5d671bf0a4869c1e9fe8ec53050fa5a79
zaid-syed <70218862+zaid-syed@users.noreply.github.com>
2020-09-12 08:31:06 -0700

    Update # Capatilizing each word in a.py

--- batch7 day 5/# Capatilizing each word in a.py
+++ batch7 day 5/# Capatilizing each word in a.py
@@ -1,6 +1,6 @@
 # Capatilizing each word in a
 x=lambda a : map(lambda z: z.capitalize(), a)
-y=["hey this is shoaib","i am from hyderabad","this program is a little complex"]
+y=["hey this is zaid","i am from hyderabad","this program is short"]
 
 c=list(map(lambda v:' '.join(v),(map(x,list(map(lambda s:s.split(),y))))))
 
